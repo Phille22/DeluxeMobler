122953c1de1c0545f0e246519fd7c39c356757ab
Philip Eriksson <phille.e.eriksson@gmail.com>
2019-01-14 11:49:58 +0100

    Första prototyp av sidan

---
+++ DeluxeMobler/Models/Furniture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeluxeMobler.Models
+{
+    public class Furniture
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Height { get; set; }
+        public int Price { get; set; }
+        public int InStock { get; set; }
+        public int NumberOfViews { get; set; }
+        public int NumberOfTimesBought { get; set; }
+        public string PictureURL { get; set; }
+    }
+
+    public class Table : Furniture
+    {
+        int NumberOfLegs { get; set; }
+        bool CanBeFold { get; set; }
+        string Shape { get; set; }
+    }
+
+    public class Bed : Furniture
+    {
+        int Softness { get; set; }
+        bool IsDouble { get; set; }
+
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Category
+    {
+
+    }
+
+    public class Cart
+    {
+
+    }
+}
