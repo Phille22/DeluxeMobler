a783e2261c47af5548d661654454e6b9727cafaa
Philip Eriksson <phille.e.eriksson@gmail.com>
2019-01-15 15:56:59 +0100

    Bilder till objekten

--- DeluxeMobler/Models/List.cs
+++ DeluxeMobler/Models/List.cs
@@ -11,8 +11,8 @@ namespace DeluxeMobler.Models
         {
             List<Furniture> FurnitureList = new List<Furniture>();
 
-            FurnitureList.Add(new Table { Name = "Sven", Price = 300 });
-            FurnitureList.Add(new Bed { Name = "Johanna", Price = 250 });
+            FurnitureList.Add(new Table { Name = "Sven", Price = 300, PictureURL = @"/Content/Images/Sofa.svg" });
+            FurnitureList.Add(new Bed { Name = "Johanna", Price = 250, PictureURL = @"\Content\Images\Skrapan,_Open_House_Stockholm_2016,_bild_10.jpg" });
 
 
             return FurnitureList;
